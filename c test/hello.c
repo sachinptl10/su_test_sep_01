0d34018fc19d77c980cc964c545ac518baba273e
sachin <sachin.patel.cg@gmail.com>
2025-09-02 18:06:18 +0530

    Added Html and C test

---
+++ c test/hello.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+
+int main() {
+
+   // question 1
+    // char ch;
+
+    // printf("character: ");
+    // scanf("%c", &ch);
+
+    // if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+    //     printf("Alphabet\n");
+    // } 
+    // else if (ch >= '0' && ch <= '9') {
+    //     printf("Digit\n");
+    // } 
+    // else {
+    //     printf("Special Symbol\n");
+    // }
+     
+    // int unit;
+    // float bill = 0;
+    // printf(" units consumed:");
+    // scanf("%d", &unit);
+
+// qstn 2
+    // if (unit <= 50) {
+    //     bill = unit * 2;
+    // } else if (unit <= 100) {
+    //     bill = unit * 3;
+    // } else if (unit <= 200) {
+    //     bill = unit* 4;
+    // } else if (unit <= 300) {
+    //     bill = unit * 5;
+    // } else if (unit  <= 500)
+    //  {
+    //     bill = unit* 6;
+    // } else {
+    //     bill = unit*8;}
+
+    
+    // bill = bill + 100;
+
+    
+    // printf("Total Bill = ₹%.2f", bill);
+
+
+   // qstn 3
+    //  int a,b,c,d,e;
+    //  scanf("%d %d %d %d %d" ,&a,&b,&c,&d,&e);
+    //   float percentage;
+    //   printf("Enter marks of 5 subjects");
+
+
+    // int total;
+    // total = a+b+c+d+e;
+    // percentage = total / 5.0;
+
+    // printf("Total Marks = %d", total);
+    // printf("Percentage = %.2f", percentage);
+
+//qstn 3
+    // if (percentage >= 90)
+    //     printf("\nGrade = A+");
+    // else if (percentage >= 80)
+    //     printf("\nGrade = A");
+    // else if (percentage >= 70)
+    //     printf("\nGrade = B");
+    // else if (percentage >= 60)
+    //     printf("\nGrade = C");
+    // else if (percentage >= 50)
+    //     printf("\nGrade = D");
+    // else if (percentage >= 40)
+    //     printf("\nGrade = E");
+    // else
+    //     printf("\nGrade = Fail");
+
+//qstn 4
+    //  int n ;
+    //  scanf ("%d",&n);
+
+    //    int result =1;
+
+    //   for (int a=1 ; a<=n ; a++)
+    //   { 
+    //     result *=a;
+        
+    //  }
+    //   printf ("%d" ,result);
+
+    
+// qstn 5 nhi aata
+
+//question 6
+// int j ;
+// scanf("%d",&j);
+// int sum =0;
+// while (j>0)
+// {
+//     int digit = j%10;
+//     sum =sum +digit;
+//     j=j/10;
+// }
+//    printf("sum %d",sum);
+
+ //qstn 7 nahi aata hai
+  
+ 
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+
+
+   
+
